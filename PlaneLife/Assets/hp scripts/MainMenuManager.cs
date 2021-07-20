5ffefc40c97edc134cd9aec111325261f38a2eda
hernpiblo <hpingleenet@gmail.com>
2021-07-20 22:07:44 +0800

    Removed skins scroll, added skin color choosing
    Modified Mainmenu UI

--- PlaneLife/Assets/hp scripts/MainMenuManager.cs
+++ PlaneLife/Assets/hp scripts/MainMenuManager.cs
@@ -7,53 +7,56 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public List<GameObject> skinList;
-    private int currentIndex;
-    private void Awake() {
-        currentIndex = PlayerPrefs.GetInt("SelectedSkin",0);
-        foreach(GameObject skin in skinList) {
-            skin.SetActive(false);
-        }
-        skinList[currentIndex].SetActive(true);
-    }
+  public FlexibleColorPicker flexibleColorPicker;
+  public Material mat;
+  private float r;
+  private float g;
+  private float b;
 
-    public void nextSkin() {
-        skinList[currentIndex].SetActive(false);    
-        currentIndex++;
-        if(currentIndex >= skinList.Count) {
-            currentIndex = 0;
-        }
-        skinList[currentIndex].SetActive(true);
-    }
+  private void Awake()
+  {
+    flexibleColorPicker.startingColor = new Color(PlayerPrefs.GetFloat("r", 255), PlayerPrefs.GetFloat("g", 255), PlayerPrefs.GetFloat("b", 255));
+    mat.color = new Color(PlayerPrefs.GetFloat("r", 255), PlayerPrefs.GetFloat("g", 255), PlayerPrefs.GetFloat("b", 255));
+  }
 
-    public void previousSkin() {
-        skinList[currentIndex].SetActive(false);
-        currentIndex--;
-        if(currentIndex < 0) {
-            currentIndex = skinList.Count - 1;
-        }
-        skinList[currentIndex].SetActive(true);
-    }
+  public void OpenColorCustomizer()
+  {
+    flexibleColorPicker.gameObject.SetActive(true);
+  }
 
-     public void startGame(int level)
+  public void CloseColorCustomizer()
+  {
+    flexibleColorPicker.gameObject.SetActive(false);
+  }
+
+  void Update()
+  {
+    mat.color = flexibleColorPicker.color;
+    r = flexibleColorPicker.color.r;
+    g = flexibleColorPicker.color.g;
+    b = flexibleColorPicker.color.b;
+  }
+
+  public void startGame(int level)
+  {
+    PlayerPrefs.SetFloat("r", r);
+    PlayerPrefs.SetFloat("g", g);
+    PlayerPrefs.SetFloat("b", b);
+    if (level == 1)
     {
-        Debug.Log(currentIndex);
-        PlayerPrefs.SetInt("SelectedSkin", currentIndex);
-        if (level == 1)
-        {
-            SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
-            PlayerPrefs.SetInt("prevLvl",1);
-        }           
-        else if (level == 2)
-        {
-            SceneManager.LoadScene("Level2", LoadSceneMode.Single);
-            PlayerPrefs.SetInt("prevLvl", 2);
-        }
-            
+      SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
+      PlayerPrefs.SetInt("prevLvl", 1);
+    }
+    else if (level == 2)
+    {
+      SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+      PlayerPrefs.SetInt("prevLvl", 2);
     }
 
-    public void ResetScore()
-    {
-        GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreBoard>().score = 0;
-    }
+  }
+
+  public void ResetScore()
+  {
+    GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreBoard>().score = 0;
+  }
 }
