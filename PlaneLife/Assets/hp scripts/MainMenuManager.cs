4358399309b35da3f62898c3ab920c8c800fa9b1
hernpiblo <hpingleenet@gmail.com>
2021-07-21 00:56:20 +0800

    Added Settings menu and volume controls

--- PlaneLife/Assets/hp scripts/MainMenuManager.cs
+++ PlaneLife/Assets/hp scripts/MainMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 
@@ -13,10 +14,21 @@ public class MainMenuManager : MonoBehaviour
   private float g;
   private float b;
 
+  public GameObject mainMenuBGM;
+  public GameObject[] mainMenuSFXarray;
+  public GameObject BGMslider;
+  private float BGMvolume;
+  public GameObject SFXslider;
+  private float SFXvolume;
+
   private void Awake()
   {
     flexibleColorPicker.startingColor = new Color(PlayerPrefs.GetFloat("r", 255), PlayerPrefs.GetFloat("g", 255), PlayerPrefs.GetFloat("b", 255));
     mat.color = new Color(PlayerPrefs.GetFloat("r", 255), PlayerPrefs.GetFloat("g", 255), PlayerPrefs.GetFloat("b", 255));
+    BGMvolume = PlayerPrefs.GetFloat("BGMvolume", 0.75F);
+    BGMslider.GetComponent<Slider>().value = BGMvolume;
+    SFXvolume = PlayerPrefs.GetFloat("SFXvolume", 0.75F);
+    SFXslider.GetComponent<Slider>().value = SFXvolume;
   }
 
   public void OpenColorCustomizer()
@@ -35,6 +47,14 @@ public class MainMenuManager : MonoBehaviour
     r = flexibleColorPicker.color.r;
     g = flexibleColorPicker.color.g;
     b = flexibleColorPicker.color.b;
+
+    BGMvolume = BGMslider.GetComponent<Slider>().value;
+    SFXvolume = SFXslider.GetComponent<Slider>().value;
+    foreach (GameObject go in mainMenuSFXarray)
+    {
+      go.GetComponent<AudioSource>().volume = SFXvolume;
+    }
+    mainMenuBGM.GetComponent<AudioSource>().volume = 0.05F * BGMvolume;
   }
 
   public void startGame(int level)
@@ -42,6 +62,10 @@ public class MainMenuManager : MonoBehaviour
     PlayerPrefs.SetFloat("r", r);
     PlayerPrefs.SetFloat("g", g);
     PlayerPrefs.SetFloat("b", b);
+
+    PlayerPrefs.SetFloat("BGMvolume", BGMvolume);
+    PlayerPrefs.SetFloat("SFXvolume", SFXvolume);
+    Debug.Log(BGMvolume);
     if (level == 1)
     {
       SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
